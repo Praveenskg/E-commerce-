c78bc5c9315fdffc6c58bb03d51f7efad208043e
Praveenskg <Praveensinghiics@gmail.com>
2023-04-06 23:39:09 +0530

    carasouel ui chaged

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,11 @@
-import Carousel from "react-bootstrap/Carousel";
 import img1 from "./assets/1st.jpg";
 import img2 from "./assets/2nd.jpg";
 import img3 from "./assets/3rd.jpg";
 import img4 from "./assets/4th.jpg";
 import img5 from "./assets/5th.jpg";
 import Map from "./Components/Map";
+import { Carousel } from "react-responsive-carousel";
+
 import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
@@ -12,7 +13,7 @@ export default function Home() {
   }, []);
   return (
     <div>
-      <Carousel variant="dark" interval={2500}>
+      {/* <Carousel variant="dark" interval={2500}>
         <Carousel.Item>
           <img
             className="d-block  w-100"
@@ -53,6 +54,23 @@ export default function Home() {
             style={{ height: 800, width: 100 }}
           />
         </Carousel.Item>
+      </Carousel> */}
+      <Carousel>
+        <div>
+          <img src={img1} className="d-block w-100" />
+        </div>
+        <div>
+          <img src={img2} className="d-block w-100" />
+        </div>
+        <div>
+          <img src={img3} className="d-block w-100" />
+        </div>
+        <div>
+          <img src={img4} className="d-block w-100" />
+        </div>
+        <div>
+          <img src={img5} className="d-block w-100 h-20" />
+        </div>
       </Carousel>
       <Map />
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
